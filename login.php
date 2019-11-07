4ada6b322a82191e0d3541d2ecea3228d05c7443
dora-0 <55905774+dora-0@users.noreply.github.com>
2019-11-08 01:15:15 +0900

    Add: Chat feature (prototype)

--- login.php
+++ login.php
@@ -17,28 +17,28 @@
 
 <body>
 <?php include("nav.php"); ?>
-    <main class="page login-page">
-        <section class="clean-block clean-form dark">
-            <div class="container">
-                <div class="block-heading">
-                    <h2 class="text-success">로그인</h2>
-                    <p>계속하려면 로그인하세요.</p>
-                </div>
-                <form>
-                    <div class="form-group"><label for="email">E-mail</label><input class="form-control item" type="email" id="email"></div><div class="form-group"><label for="password">비밀번호</label><input class="form-control" type="password" id="password"></div>
-                    <div class="form-group">
-                        <div class="form-check"><input class="form-check-input" type="checkbox" id="checkbox"><label class="form-check-label" for="checkbox">자동 로그인</label></div>
-                    </div><button class="btn btn-success btn-block" type="submit">로그인</button>
-                    <button class="btn btn-block btn-outline-success" type="submit">회원가입</button></form>
+<main class="page login-page">
+    <section class="clean-block clean-form dark">
+        <div class="container">
+            <div class="block-heading">
+                <h2 class="text-success">로그인</h2>
+                <p>계속하려면 로그인하세요.</p>
             </div>
-        </section>
-    </main>
+            <form>
+                <div class="form-group"><label for="email">E-mail</label><input class="form-control item" type="email" id="email"></div><div class="form-group"><label for="password">비밀번호</label><input class="form-control" type="password" id="password"></div>
+                <div class="form-group">
+                    <div class="form-check"><input class="form-check-input" type="checkbox" id="checkbox"><label class="form-check-label" for="checkbox">자동 로그인</label></div>
+                </div><button class="btn btn-success btn-block" type="submit">로그인</button>
+                <button class="btn btn-block btn-outline-success" type="submit">회원가입</button></form>
+        </div>
+    </section>
+</main>
 <?php include("footer.php"); ?>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.js"></script>
-    <script src="assets/js/smoothproducts.min.js"></script>
-    <script src="assets/js/theme.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.js"></script>
+<script src="assets/js/smoothproducts.min.js"></script>
+<script src="assets/js/theme.js"></script>
 </body>
 
 </html>
