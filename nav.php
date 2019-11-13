6ca9c5014de800fc028719d4ffaafbae2e6b6d4b
dora-0 <55905774+dora-0@users.noreply.github.com>
2019-11-13 14:04:54 +0900

    Add: hide .php feature

--- nav.php
+++ nav.php
@@ -4,11 +4,11 @@
             <div class="collapse navbar-collapse"
                  id="navcol-1">
                 <ul class="nav navbar-nav ml-auto">
-                    <li class="nav-item" role="presentation"><a class="nav-link <?php if ($parent === "/var/www/html/public/dora-web/index.php") echo "active"; ?>" href="index.php">Home</a></li>
-                    <li class="nav-item" role="presentation"><a class="nav-link <?php if ($parent === "/var/www/html/public/dora-web/about.php") echo "active"; ?>" href="about.php">Features</a></li>
-                    <li class="nav-item" role="presentation"><a class="nav-link <?php if ($parent === "/var/www/html/public/dora-web/blog-post-list.php") echo "active"; ?>" href="blog-post-list.php">Blog</a></li>
-                    <li class="nav-item" role="presentation"><a class="nav-link <?php if ($parent === "/var/www/html/public/dora-web/chat.php") echo "active"; ?>" href="chat.php">Chat</a></li>
-                    <li class="nav-item" role="presentation"><a class="nav-link <?php if ($parent === "/var/www/html/public/dora-web/login.php") echo "active"; ?>" href="login.php">Login</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link <?php if ($parent === "/var/www/html/public/dora-web/index") echo "active"; ?>" href="index">Home</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link <?php if ($parent === "/var/www/html/public/dora-web/about") echo "active"; ?>" href="about">Features</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link <?php if ($parent === "/var/www/html/public/dora-web/blog-post-list") echo "active"; ?>" href="blog-post-list">Blog</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link <?php if ($parent === "/var/www/html/public/dora-web/chat") echo "active"; ?>" href="chat">Chat</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link <?php if ($parent === "/var/www/html/public/dora-web/login") echo "active"; ?>" href="login">Login</a></li>
                 </ul>
             </div>
         </div>
