e009c7acbbf69c3d24a48d1260d9fb75f912aea1
dora-0 <55905774+dora-0@users.noreply.github.com>
2019-11-14 09:26:06 +0900

    일부 코드를 jQuery 문법으로 변경

--- chat_client.php
+++ chat_client.php
@@ -7,13 +7,13 @@
             </div>
             <input class="inputMessage" placeholder="여기에 입력..."/>
         </li>
-        <li class="login page">
+        <li id="login-section" class="login page">
             <div class="form">
                 <h3 class="title">닉네임을 입력하세요.</h3>
                 <input class="usernameInput" type="text" maxlength="14" />
             </div>
         </li>
-        <li class="load page">
+        <li id="load-section" class="load page">
             <div id="notify-section" class="section">
                 <img alt='연결 중' style='max-width: 128px' src='assets/img/antenna.png' />
                 <h3 class="title">서버에 연결하는 중...</h3>
@@ -21,14 +21,13 @@
         </li>
     </ul>
     <script>
-        //TODO: JQuery 문법으로 바꾸기
         function init_login() {
-            document.getElementsByClassName('login')[0].style.display = "block";
-            document.getElementsByClassName('load')[0].style.display = "none";
+            $("#login-section").css('display', 'block');
+            $("#load-section").css('display', 'none');
         }
 
         function load_error() {
-            document.getElementById("notify-section").innerHTML = "<img alt='연결 실패' style='max-width: 128px' src='assets/img/broken-link.png' /><br/><h3 class='title'>연결에 실패했습니다.</h3>";
+            $("#notify-section").html("<img alt='연결 실패' style='max-width: 128px' src='assets/img/broken-link.png' /><br/><h3 class='title'>연결에 실패했습니다.</h3>");
         }
     </script>
     <script src="https://azure.mandora.xyz:3001/socket.io/socket.io.js" onload="init_login()" onerror="load_error()"></script>
