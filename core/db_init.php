8b3e50cc9557e4c4e11061a4e18a2b68390b75f2
dora-0 <55905774+dora-0@users.noreply.github.com>
2019-11-14 09:40:40 +0900

    Update db_init.php

--- core/db_init.php
+++ core/db_init.php
@@ -1,10 +1,14 @@
 <?php
-$host = "azure.mandora.xyz";
+$host = "localhost";
 $user = "dora-web";
 $password = "GSGMFGWHOjrvQwZa";
 $dbname = "dora-web";
-$port = 33060;
+$port = 3306;
 
 $link = mysqli_connect($host, $user, $password, $dbname, $port);
+
+if (!$link) {
+    echo "Error: DB 서버에 접속하는 데 실패했습니다.";
+}
 unset($host, $user, $password, $dbname, $port);
 ?>
