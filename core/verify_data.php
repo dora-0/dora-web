ad2e44df6cc7f9d9b28296bed2acbcc399727d9c
dora-0 <55905774+dora-0@users.noreply.github.com>
2019-11-15 13:43:55 +0900

    Fix: add Access-Control-Allow-Origin,Credentials

--- core/verify_data.php
+++ core/verify_data.php
@@ -4,6 +4,8 @@ if ($_SERVER["REQUEST_METHOD"] != "POST") {
 }
 
 session_start();
+header("Access-Control-Allow-Origin: https://azure.mandora.xyz");
+header("Access-Control-Allow-Credentials: true");
 $data = new stdClass();
 
 function verify_data($string, $mode) {
