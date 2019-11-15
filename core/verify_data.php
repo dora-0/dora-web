bc6e2b8389ad41fb1c8e1f248a21f6ab9f25368e
dora-0 <55905774+dora-0@users.noreply.github.com>
2019-11-15 09:49:27 +0900

    Add: 아이디, 닉네임 중복 체크 기능

--- core/verify_data.php
+++ core/verify_data.php
@@ -1,9 +1,33 @@
 <?php
-//require_once "../core/db_init.php";
+require_once "db_init.php";
 header("Content-Type:application/json");
 
 if ($_SERVER["REQUEST_METHOD"] != "POST") {
     exit;
+}
+
+$data = new stdClass();
+$data->is_dupe = new stdClass();
+$data->is_dupe->user_id = false;
+$data->is_dupe->nickname = false;
+
+$query = "SELECT * FROM `users` WHERE user_id='".$_POST["user_id"]."'";
+if ($result = mysqli_query($link, $query, MYSQLI_USE_RESULT)) {
+    if ($row = mysqli_fetch_object($result)) {
+//        $user_id = $row->user_id;
+        $data->is_dupe->user_id = true;
+        mysqli_free_result($result);
+        unset($query);
+    }
+}
+
+$query = "SELECT * FROM `users` WHERE nickname='".$_POST["nickname"]."'";
+if ($result = mysqli_query($link, $query, MYSQLI_USE_RESULT)) {
+    if ($row = mysqli_fetch_object($result)) {
+        $data->is_dupe->nickname = true;
+        mysqli_free_result($result);
+        unset($query);
+    }
 }
 
 function verify_data($string, $mode) {
@@ -25,6 +49,11 @@ function verify_data($string, $mode) {
                     $data->verified = false;
                     $data->outMsg = "<span class='text-danger'>아이디의 길이는 최대 20자까지만 허용됩니다.</span>";
                 }
+
+                if ($data->is_dupe->user_id) {
+                    $data->verified = false;
+                    $data->outMsg = "<span class='text-danger'>이미 존재하는 아이디입니다.</span>";
+                }
                 break;
             case "nickname":
                 $data->outMsg = "<span class='text-success'>사용 가능한 닉네임입니다.</span>";
@@ -37,6 +66,11 @@ function verify_data($string, $mode) {
                 if (strlen($string) > 20) {
                     $data->verified = false;
                     $data->outMsg = "<span class='text-danger'>닉네임의 길이는 최대 20자까지만 허용됩니다.</span>";
+                }
+
+                if ($data->is_dupe->nickname) {
+                    $data->verified = false;
+                    $data->outMsg = "<span class='text-danger'>이미 존재하는 닉네임입니다.</span>";
                 }
                 break;
             default:
@@ -51,15 +85,22 @@ function verify_data($string, $mode) {
     return $data;
 }
 
-if ($_GET["mode"] === "ajax") {
-    $data = new stdClass();
+$data->user_id = verify_data($_POST["user_id"], "user_id");
+$data->nickname = verify_data($_POST["nickname"], "nickname");
 
-    $data->user_id = verify_data($_POST["user_id"], "user_id");
-    $data->nickname = verify_data($_POST["nickname"], "nickname");
-//$arr[] = $data;
-//echo json_encode($arr);
+if ($_GET["mode"] === "ajax") {
     echo json_encode($data);
-//unset($arr, $data);
     unset($data);
+}
+else if ($_GET["mode"] === "process") {
+    if (!($data->user_id->verified && $data->nickname->verified)) {
+        echo "<script>alert('비정상적인 접근이 감지되었습니다.');</script>";
+        header('HTTP/1.1 301 Moved Permanently');
+        header('Location: https://azure.mandora.xyz/dora-web/');
+        exit;
+    }
+    else {
+
+    }
 }
 ?>
