cc893d153e3f6ee2b5f2bc2ce106af9e2b3211ce
dora-0 <55905774+dora-0@users.noreply.github.com>
2019-11-15 13:54:45 +0900

    Fix: db_init.php, join.php

--- core/db_init.php
+++ core/db_init.php
@@ -1,14 +1,13 @@
 <?php
-session_start();
 $host = "localhost";
 $user = "dora-web";
 $password = "GSGMFGWHOjrvQwZa";
 $dbname = "dora-web";
 $port = 3306;
 
-$_SESSION['link'] = mysqli_connect($host, $user, $password, $dbname, $port);
+$link = mysqli_connect($host, $user, $password, $dbname, $port);
 
-if (!$_SESSION['link']) {
+if (!$link) {
     echo "Error: DB 서버에 접속하는 데 실패했습니다.";
 }
 unset($host, $user, $password, $dbname, $port);
--- join.php
+++ join.php
@@ -1,6 +1,10 @@
 <?php $parent = __FILE__; ?>
 <?php
+    session_start();
     require_once "core/db_init.php";
+
+    //register session variables
+    $_SESSION['link'] = $link;
 
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         require_once "core/verify_data.php";
