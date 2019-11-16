4c2cb058053b1dc1b043374fbc98dfb3b13323fa
dora-0 <55905774+dora-0@users.noreply.github.com>
2019-11-16 23:48:52 +0900

    Fix: Invalid URL, add Error Message (and fix typo)

--- core/db_init.php
+++ core/db_init.php
@@ -8,8 +8,8 @@ $port = 39148;
 $link = mysqli_connect($host, $user, $password, $dbname, $port);
 
 if (!$link) {
-    trigger_error("Failed to connect to DB server. mysqli_error() reported as follows: ".mysqli_error($link), E_USER_WARNING);
-    echo "Error: DB 서버에 접속하는 데 실패했습니다.";
+    trigger_error("DB server connection failed. mysqli_error() reported as follows: ".mysqli_error($link), E_USER_WARNING);
+//    echo "Error: DB 서버에 접속하는 데 실패했습니다.";
 }
 unset($host, $user, $password, $dbname, $port);
 ?>
