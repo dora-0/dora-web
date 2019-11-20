b4b74cadd754bc3ba58dddbf7517995828f35054
dora-0 <55905774+dora-0@users.noreply.github.com>
2019-11-20 09:37:44 +0900

    Fix: member.js: mode=join -> submit

--- assets/js/member.js
+++ assets/js/member.js
@@ -2,7 +2,7 @@ function verify_data(mode = null) {
     var params = $("#JoinForm").serialize();
     var verified = false;
     var requestURI = null;
-    if (mode === "join") {
+    if (mode === "submit") {
         requestURI = "core/verify_data?mode=join&type=ajax&db_check=true";
         $('#verify_user_id').html("<span class='text-warning'>아이디 중복 검사 중 ...</span>");
         $('#verify_nickname').html("<span class='text-warning'>닉네임 중복 검사 중 ...</span>");
