e0c38eddf92e675dcdf718fb051bf3a9d8d5c433
dora-0 <55905774+dora-0@users.noreply.github.com>
2019-11-25 20:05:56 +0900

    Add: 회원정보 수정 기본 디자인 구현

--- login.php
+++ login.php
@@ -75,7 +75,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 <input type='hidden' name='redirect_url' value='<?=$_POST["redirect_url"]?>' />
                 <?php endif; ?>
                 <div class="form-group"><label for="username">아이디</label><input class="form-control item" type="text" id="username" name="user_id"><label id="verify_user_id"></label></div>
-                <div class="form-group"><label for="password">비밀번호</label><input class="form-control" type="password" id="password" name="password"><label id="verify_password"></label></div>
+                <div class="form-group"><label for="password">비밀번호</label><input class="form-control item" type="password" id="password" name="password"><label id="verify_password"></label></div>
                 <!--                <div class="form-group">-->
                 <!--                    <div class="form-check"><input class="form-check-input" type="checkbox" id="checkbox"><label class="form-check-label" for="checkbox">자동 로그인</label></div>-->
                 <!--                </div>-->
